991907961f2b2f60946a42b464f1385d99172e2e
popcornmix <popcornmix@gmail.com>
2015-05-31 11:54:57 +0100

    Optimise juliatrap

--- screensaver.shadertoy/resources/juliatrap.frag.glsl
+++ screensaver.shadertoy/resources/juliatrap.frag.glsl
@@ -10,17 +10,16 @@
 
 void mainImage( out vec4 fragColor, in vec2 fragCoord )
 {
-	vec2 z = 1.15*(-iResolution.yx+2.0*fragCoord.yx)/iResolution.y;
+	vec2 z = 1.15*(-iResolution.xy+2.0*fragCoord.xy)/iResolution.y;
 
 	vec2 an = 0.51*cos( vec2(0.0,1.5708) + 0.1*iGlobalTime ) - 0.25*cos( vec2(0.0,1.5708) + 0.2*iGlobalTime );
 
-	float f = 1e20;
-	for( int i=0; i<17; i++ ) 
+	for( int i=0; i<17; i++ )
 	{
 		z = vec2( z.x*z.x-z.y*z.y, 2.0*z.x*z.y ) + an;
-		f = min( f, dot(z,z) );
 	}
 	
+	float f = min(1e20, dot(z,z));
 	f = 1.0+log(f)/16.0;
 
 	fragColor = vec4(f,f*f,f*f*f,1.0);
