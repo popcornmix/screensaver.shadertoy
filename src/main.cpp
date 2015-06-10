96309c84cd0e2ab6d5c3a27e0159d342d80b20ff
popcornmix <popcornmix@gmail.com>
2015-06-10 23:06:03 +0100

    Add fireball

--- src/main.cpp
+++ src/main.cpp
@@ -60,6 +60,7 @@ const std::vector<Preset> g_presets =
    {"Cellular",                        "cellular.frag.glsl",             -1,-1,-1,-1},
    {"Coloured Circles",                "colorcircles.frag.glsl",         -1,-1,-1,-1},
    {"Fast Clouds",                     "fastclouds.frag.glsl",           12,-1,-1,-1},
+   {"Fireball",                        "fireball.frag.glsl",             14,-1,-1,-1},
    {"Flaring",                         "flaring.frag.glsl",              12,-1,-1,-1},
    {"Fractal Tiling",                  "fractaltiling.frag.glsl",        -1,-1,-1,-1},
    {"Glowing Thing",                   "glowingthing.frag.glsl",         -1,-1,-1,-1},
