7e9c95b911bf59825c98a55513e148d1e639c756
popcornmix <popcornmix@gmail.com>
2015-06-10 21:11:26 +0100

    Add Fractal Tiling

--- src/main.cpp
+++ src/main.cpp
@@ -61,6 +61,7 @@ const std::vector<Preset> g_presets =
    {"Coloured Circles",                "colorcircles.frag.glsl",         -1,-1,-1,-1},
    {"Fast Clouds",                     "fastclouds.frag.glsl",           12,-1,-1,-1},
    {"Flaring",                         "flaring.frag.glsl",              12,-1,-1,-1},
+   {"Fractal Tiling",                  "fractaltiling.frag.glsl",        -1,-1,-1,-1},
    {"Interstellar",                    "stellar.frag.glsl",              14,-1,-1,-1},
    {"Interweaving Sine bands",         "sinebands.frag.glsl",            -1,-1,-1,-1},
    {"Juliasm",                         "juliasm.frag.glsl",              -1,-1,-1,-1},
